c335ac26cdb486746c5f3fc608991dfca9dde6c5
alimohammadi <alimohammadi02321@gmail.com>
2025-09-20 15:22:20 +0330

    Improve/ add & complete getPrice function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -23,6 +23,7 @@ pragma solidity ^0.8.18;
 //     view
 //     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 // }
+
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
@@ -44,9 +45,15 @@ contract FundMe{
 
     // function withdraw() public payable {}
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+       (,int256 price,,,) =  priceFeed.latestRoundData(); // Price of ETH in terms of USD
+        
+
+        return uint256(price) *1e10;
     }
 
     function getConvertionRate() public {
