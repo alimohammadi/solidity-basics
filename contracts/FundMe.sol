92d33cdbf29373689348ba97fddc61e3a38b74ad
alimohammadi <alimohammadi02321@gmail.com>
2025-09-20 17:12:17 +0330

    Feature/ add library for uint256

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,6 +5,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {PriceConverter} from "./PriceConvertor.sol";
 
 // solhint-disable-next-line interface-starts-with-i
 // interface AggregatorV3Interface {
@@ -24,19 +25,24 @@ pragma solidity ^0.8.18;
 //     returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 // }
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
-    
-    uint256 public minimumUsd = 5;
+    using PriceConverter for uint256;
+    uint256 public minimumUsd = 5 * 1e18;
+
+    address[] public funders;
+    mapping(address founder=>uint256 amountFunded) public addressToAmountFounded;
 
     function fund() public payable{
         // Allow user to send money
         // Have a minimum $ sent
 
         // How do we send ETH to this contract?
-        require(msg.value >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH
+        // msg.value.getConversionRate();
 
+        funders.push(msg.sender);
+        addressToAmountFounded[msg.sender] = addressToAmountFounded[msg.sender] + msg.value;
         // What is revert?
         // Undo any actions that have been done, and send the remaining gas back
         // If you send fail transaction you spent gas
@@ -44,32 +50,5 @@ contract FundMe{
     }
 
     // function withdraw() public payable {}
-
-    function getPrice() public view returns (uint256) {
-        // AggregatorV3Interface();
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed =  AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-       (,int256 price,,,) =  priceFeed.latestRoundData(); // Price of ETH in terms of USD
-        
-        return uint256(price) *1e10;
-    }
-
-    function getConvertionRate(uint256 ethAmount) public view returns (uint256){
-        // 1 ETH?
-        // 2000_000000000000000000
-        uint256 ethPrice = getPrice();
-
-        // (2000_000000000000000000 * 1_000000000000000000) / 1e18
-        // 2000 $ = 1 ETH
-        uint256 ethAmountInUSD =  (ethPrice * ethAmount) / 1e18;
-
-        return ethAmountInUSD;
-    }
-
-    function getVersion() public view returns (uint256){
-      return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
-
 }
 
