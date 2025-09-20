52aed0916b1c03388bf18f9cb59f497428832fd9
alimohammadi <alimohammadi02321@gmail.com>
2025-09-20 10:57:44 +0330

    Improve/ complete transaction flow

--- contracts/StorageFactrory.sol
+++ contracts/StorageFactrory.sol
@@ -1,51 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract SimpleStorage {
-    uint256 public myFavoriteNumber; // 0
-
-    uint256[] listOfFavoriteNumbers; // []
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
-
-    // dynamic array 
-    Person[] public listOfPeople; //[]
-
-    // static array 
-    Person[4] public listOfPeople2; //[]
-
-    // Person public myfriend = Person(7, "Ali");
-    Person public myfriend = Person({favoriteNumber: 7, name: "Ali"});
-    Person public myfriend2 = Person({favoriteNumber: 7, name: "Ali2"});
-    Person public myfriend3 = Person({favoriteNumber: 7, name: "Ali3"});
-
-    // mapping (key => value)
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        Person memory newPerson = Person(_favoriteNumber, _name);
-        listOfPeople.push(newPerson);
-        nameToFavoriteNumber[_name] = _favoriteNumber; // store mapping too
-    }
-
-    function store(uint256 _favoriteNumber) public {
-        myFavoriteNumber = _favoriteNumber;
-    } 
-
-    // view
-    function retrieve() public view returns (uint256) {
-        return myFavoriteNumber;
-    }
-}
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory{
     // type visibility name
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
 
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        // Addr & ABI(Application Binary Interface)
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage mySimpleStorage =listOfSimpleStorageContracts[_simpleStorageIndex];
+        return mySimpleStorage.retrieve();
     }
 }
