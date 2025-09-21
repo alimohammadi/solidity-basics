621d907045596ab24229a13abf64ae1ba6f80ae4
alimohammadi <alimohammadi02321@gmail.com>
2025-09-21 15:26:13 +0330

    Improve/ add three transfer eth methods

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -56,6 +56,24 @@ contract FundMe{
            address funder = funders[funderIndx];
            addressToAmountFounded[funder] = 0;
         }
+
+        // reset the array
+        funders = new address[](0);
+
+        // withdraw the funds
+
+        // transfer 
+        // msg.sender <=> address, payable(msg.sender) <=> payable address
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send 
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        
+        // call
+       (bool callSuccess, ) = payable(msg.sender).call{value:address(this).balance}("");
+
+       require(callSuccess, "Call failed");
     }
 }
 
